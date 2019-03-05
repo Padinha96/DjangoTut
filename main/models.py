2b7027735f9592a00498fd19aed864e62d0b6fba
JOSÉ TEIXEIRA <a52107@ualg.pt>
2019-03-05 23:30:26 +0000

    fixed trailing lines

--- main/models.py
+++ main/models.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from django.db import models
-from tinymce import TinyMCE
 
 # Create your models here.
 class Tutorial(models.Model):
@@ -10,9 +9,3 @@ class Tutorial(models.Model):
 
     def __str__(self):
         return self.tutorial_title
-
-
-
-
-
-
