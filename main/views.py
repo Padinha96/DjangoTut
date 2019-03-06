4a7aebb4000590130a665ca382aba18866a8500c
JOSÉ TEIXEIRA <a52107@ualg.pt>
2019-03-06 16:47:04 +0000

    commit son

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.utils import translation
 from .models import Tutorial
 
 # Create your views here.
 def homepage(request):
+    translation.activate('en')
     return render(request=request,
                   template_name="main/home.html",
                   context={"tutorials": Tutorial.objects.all}
