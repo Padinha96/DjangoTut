cd205a426e62fd9a2370d35396263deec1cbef45
JOSÉ TEIXEIRA <a52107@ualg.pt>
2019-03-09 22:05:43 +0000

    tut9complete

--- main/admin.py
+++ main/admin.py
@@ -1,19 +1,25 @@
 from tinymce.widgets import TinyMCE
 from django.contrib import admin
 from django.db import models
-from .models import Tutorial
+from .models import Tutorial, TutorialSeries, TutorialCategory
 # Register your models here.
 
 class TutorialAdmin(admin.ModelAdmin):
 
     fieldsets = [
-        ("Title/Date", {"fields": ["tutorial_title", "tutorial_published"]}),
-        ("Content", {"fields": ["tutorial_content"]})
+        ("Title/Date", {"fields": ["title", "published"]}),
+        ("URL", {"fields": ["slug"]}),
+        ("Series", {"fields": ["series"]}),
+        ("Content", {"fields": ["content"]})
     ]
 
     formfield_overrides = {
         models.TextField: {'widget' : TinyMCE()}
     }
+
+admin.site.register(TutorialSeries)
+
+admin.site.register(TutorialCategory)
 
 
 admin.site.register(Tutorial, TutorialAdmin)
